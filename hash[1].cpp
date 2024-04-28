0c4f3b791029229ed8b4a4754746e6eea00bc8db
Ruchi Pagar <146519515+ruchi0212@users.noreply.github.com>
2024-04-28 19:49:24 +0530

    Update hash[1].cpp

--- hash[1].cpp
+++ hash[1].cpp
@@ -1,125 +1,144 @@
 
-
-
 #include<iostream>
 using namespace std;
-class hash_table
+class HashTable
 {
-	public:
-	int key;
-	long mob_no;
+public:
+int index;
+long int mobile;
 };
-class hashing
+class Hashing
 {
-	hash_table h[10];
-	public:
-		hashing()
-		{
-			for(int i=0;i<10;i++)
-			{
-				h[i].key=i;
-				h[i].mob_no=-1;
-			}
-		}
-	void insert(int probe_choice);
-	int linear_probing(int collision_position);
-	int quadratic_probing(int collision_position);
-	void display();
+HashTable h[10];
+public:
+Hashing()
+{
+for(int i=0;i<10;i++)
+{
+h[i].index=i;
+h[i].mobile=-1;
+}
+}
+void display();
+void insert(int);
+void search();
+int LinearProbing(int);
+int QuadraticProbing(long int);
 };
-void hashing::insert(int probe_choice)
+int Hashing::QuadraticProbing(long int key)
 {
-	int temp,position;
-	int collision_position,temp_position;
-	cout<<"enter mobile number:";
-	cin>>temp;
-	position=temp%10;
-	if(h[position].mob_no==-1)
-	{
-	 	h[position].mob_no=temp;
-	  
-	}
-	else if(probe_choice==1)   		  //linear probing 
-	{
-		temp_position=linear_probing(position);
-		h[temp_position].mob_no=temp;
-	}
-	else if(probe_choice==2)			//quadratic probing
-	{
-		int collision_position=position;
-		int temp_position=quadratic_probing(collision_position);
-		h[temp_position].mob_no=temp;
-	}
+int a;
+for(int j=0;j<10;j++)
+{
+a= (key + (j*j))%10;
+if(h[a].mobile==-1)
+{
+return a;
 }
-int hashing::linear_probing(int position)
-{
-	int i;
-	for(i=position;i<10;i++)
-	{
-		if(h[i].mob_no==-1)
-		{
-			return i;
-		}
-		if(i==9) 
-		{
-			i=-1;      						//i value 9 to 0
-		}	
-	}
 }
-int hashing::quadratic_probing(int collision_position)
-{
-	int position,temp,i;
-	position=(h[temp].mob_no+i*i)%10;
-	if(h[position].mob_no==-1)
-	{
-		h[position].mob_no=temp;
-	}
 }
-
-void hashing::display()
+int Hashing::LinearProbing(int collision_position)
 {
-	for(int i=0;i<10;i++)
-	{
-		cout<<h[i].key<<"\t"<<h[i].mob_no<<endl;
- 	   
-	}
+for(int i=collision_position;i<10;i++)
+{
+if(h[i].mobile==-1)
+return i;
+if(i==9)
+i = -1;
+}
+}
+void Hashing::display()
+{
+for(int i=0;i<10;i++)
+{
+cout<<h[i].index;
+cout<<" "<<h[i].mobile;
+cout<<endl;
+}
+}
+void Hashing::insert(int probchoice)
+{
+long int key;
+int position;
+cout<<"\nEnter mobile number to insert in to hash table : ";
+cin>>key;
+position = key % 10;
+cout<<"\nPosition = "<<position;
+if(h[position].mobile==-1)
+{
+h[position].mobile=key;
+}
+else if(probchoice ==1)// Linear Probing collision.
+{
+int temp_position;
+temp_position = LinearProbing(position);
+h[temp_position].mobile=key;
+}
+else if(probchoice ==2)// Quadratic Probing for collision.
+{
+int temp_position;
+temp_position = QuadraticProbing(key);
+h[temp_position].mobile=key;
+}
+}
+void Hashing::search()
+{
+long int key;
+int position,i;
+cout<<"\nEnter mobile number to search in the hash table : ";
+cin>>key;
+position = key % 10;
+if(h[position].mobile==key)
+{
+cout<<"\nGiven mobile number is found in the hash table ";
+}
+else
+{
+for(i = 0; i<10;i++)
+{
+if(h[i].mobile==key)
+{
+cout<<"\nGiven mobile number is found in the hash table ";
+break;
+}
+}
+if(i == 10)
+{
+cout<<"\nGiven mobile number is not found in the hash table ";
+}
+}
 }
 int main()
 {
-	hashing h1;
-	int ch,probe_choice;
-	do
-	{
-	cout<<"*****Menu*****";
-	cout<<"\n1.Insert a data.";
-	cout<<"\n2.Display data.";
-	cout<<"\n3.Exit.";
-	cout<<"\nEnter a choice:";
-	cin>>ch;
-	switch(ch)
-	{
-		case 1:
-		{
-			cout<<"\n1.linear probing. \n2.Quadratic probing.";
-			cout<<"\nEnter a choice:";
-			cin>>probe_choice;
-			h1.insert(probe_choice);
-		}
-		break;
-	
-		case 2:
-			h1.display();
-			break;
-	
-		case 3:
-			cout<<"End of program......";
-			break;
-	
-	}
-	
-	}while(ch!=4);
-	return 0;
+Hashing H;
+int ch;
+do
+{
+cout<<"\n Menu";
+cout<<"\n 1.insert";
+cout<<"\n 2.display";
+cout<<"\n 3.search";
+cout<<"\n 5. exit";
+cout<<"\n Enter your choice : ";
+cin>>ch;
+switch(ch)
+{
+case 1 : //insert
+int probchoice;
+cout<<"\nEnter \n1 for LinearProbing and \n2 for Quadratic probing";
+cin>>probchoice;
+H.insert(probchoice);
+break;
+case 2 : //display
+H.display();
+break;
+case 3 : //display
+H.search();
+break;
+default:
+cout<<"\nWrong choice :";
+break;
 }
-
-
-
-/
+}while(ch != 5);
+return 0;
+}
